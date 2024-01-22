1c92d7e61d3e98eb0e41299d084bec8d1d9b5641
erfan mahmoudi <erfanmahmudi1383@gmail.com>
2024-01-22 19:50:57 +0330

    Data files added

--- src/DecisionTreeClassifier.java
+++ src/DecisionTreeClassifier.java
@@ -1,13 +1,15 @@
 public class DecisionTreeClassifier {
-
     float [][]data;
     float []labels;
-    public DecisionTreeClassifier(float [] [] data , float [] labels)
-    {
+    public DecisionTreeClassifier(float [] [] data , float [] labels) {
 
     }
-    float [] predictAll(float[][] , int depth){
-        ;
+
+    public float[] predictAll(float[][] data, int depth) {
+        return new float[0];
     }
-    float accuracy(int [] labels , int [] labels_predicted);
+
+    public float accuracy(int[] labels, int[] labels_predicted) {
+        return 0;
+    }
 }
