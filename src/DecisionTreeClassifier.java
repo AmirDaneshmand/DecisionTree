5b317f52812923318090cbc6c7c42d017a452e3e
AmirReza <d1382.1392@gmail.com>
2024-01-22 19:36:06 +0330

    make 102

--- src/DecisionTreeClassifier.java
+++ src/DecisionTreeClassifier.java
@@ -1,2 +1,13 @@
 public class DecisionTreeClassifier {
+
+    float [][]data;
+    float []labels;
+    public DecisionTreeClassifier(float [] [] data , float [] labels)
+    {
+
+    }
+    float [] predictAll(float[][] , int depth){
+        ;
+    }
+    float accuracy(int [] labels , int [] labels_predicted);
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,18 @@
 public class Tree {
+    int depth;
+
+    public int getDepth() {
+        return depth;
+    }
+
+    void createTree(float[][] data, float[] labels){}
+
+    float Entropy(float[] labels) {
+        return 0;
+    }
+
+    float iGain(int pEntropy, float[] weight, float[] entropies) {
+
+        return 0;
+    }
 }
